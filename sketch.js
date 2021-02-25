432ec19b4b8ac9098e7f20778e69d8691f2fa55a
NAISHA-AGARWAL <72607821+NAISHA-AGARWAL@users.noreply.github.com>
2021-02-25 13:28:46 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,32 +1,48 @@
-var paper;
-var ellipse;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-function preload()
-{
+var dustbinObj, paperObject,groundObject	
+var world;
 
-}
+//function preload()
+//{
+
+//}
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
 	engine = Engine.create();
 	world = engine.world;
-Engine.run(engine);
-  paper= newPaper(20,20,20);
+	dustbinObj=new Dustbin(1200,650);
+	paperObject=new Paper(200,450,40);
+	groundObject=new Ground(width/2,670,width,20);
+  Engine.run(engine);
 }
-
 
 
 function draw() {
+  rectMode(CENTER);
   background(0);
-  Engine.update(engine);
-  paper.display();
  
+  dustbinObj.display();
+  paperObject.display();
+  groundObject.display();
+  
+ 
+  
+  
  
 }
 
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
 
-
+    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+    
+  	}
+}
